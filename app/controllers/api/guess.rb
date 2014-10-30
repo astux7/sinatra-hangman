a02f2d5ceaacc9fa64806126db8c539c783234d1
Asta Bevainyte <asta.bevainyte@onthebeach.co.uk>
2014-10-30 15:45:50 +0000

    api request

--- app/controllers/api/guess.rb
+++ app/controllers/api/guess.rb
@@ -2,7 +2,7 @@
 # helpers Sinatra::JSON
 post "/api/" do
   # @blanket_view = hangman_start(params[:level].to_sym)
-   @result = "hi".to_json
+   @result = request.body.to_json
    erb :result, :layout => :api_layout
   # json({:foo => 'bar'}, :encoder => :to_json, :content_type => :js)
   #json({:result => @result}, :encoder => :to_json, :content_type => :js)
